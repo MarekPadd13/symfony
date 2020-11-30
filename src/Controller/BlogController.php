b5728d54cc271e35c0d57d4487c566fb6d368152
MarekPadd13 <markpdd13@list.ru>
2020-11-30 11:50:30 +0300

    first

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @return Response
+     * @Route("/blog", name="blog_list", methods={"GET","HEAD"})
+     */
+    public function list(): Response
+    {
+        return $this->render('lucky/number.html.twig', ['number'=> 125]);
+    }
+
+    /**
+     * @param $id
+     * @return Response
+     * @Route("/blog/view/{id?1}",
+     *      name="blog_view",
+     *     methods={"GET","HEAD"},
+     *     requirements={"id"="\d+"},
+     *    condition="context.getMethod() in ['GET', 'HEAD'] and request.headers.get('User-Agent') matches '/firefox/i'"
+     * )
+     */
+    public function view(int $id): Response
+    {
+        return $this->render('lucky/number.html.twig', ['number'=> $id]);
+    }
+
+    /**
+     * @return RedirectResponse
+     * @Route("/blog/index", name="blog_index", methods={"GET","HEAD"})
+     */
+
+    public function index(): RedirectResponse
+    {
+        // redirects to the "homepage" route
+        //return $this->redirectToRoute('blog_view', ['id'=> 45]);
+
+        // redirectToRoute is a shortcut for:
+        // return new RedirectResponse($this->generateUrl('homepage'));
+
+        // does a permanent - 301 redirect
+//        return $this->redirectToRoute('homepage', [], 301);
+//
+//        // redirect to a route with parameters
+//        return $this->redirectToRoute('app_lucky_number', ['max' => 10]);
+//
+//        // redirects to a route and maintains the original query string parameters
+//        return $this->redirectToRoute('blog_show', $request->query->all());
+//
+//        // redirects externally
+         return $this->redirect('http://symfony.com/doc');
+    }
+}
---
+++ src/Controller/MakeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MakeController extends AbstractController
+{
+    /**
+     * @Route("/make", name="make")
+     */
+    public function index(): Response
+    {
+        return $this->render('make/index.html.twig', [
+            'controller_name' => 'MakeController',
+        ]);
+    }
+
+    /**
+     * @Route("/make/view/{id}", name="make_view")
+     * @param int $id
+     * @return Response
+     */
+    public function view($id = 1): Response
+    {
+        if($id == 2) {
+          //  throw  $this->createNotFoundException("Такой страницы не существует");
+            $this->addFlash(
+                'notice',
+                'Your changes were saved!'
+            );
+            return $this->redirectToRoute('brand_new');
+        }
+        return $this->render('make/index.html.twig', [
+            'controller_name' => 'MakeController',
+        ]);
+    }
+}
